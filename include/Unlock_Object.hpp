b1603e7d5d68c13fd448dd566260e0219698d242
Jair-F <jair.fehlauer@gmail.com>
2022-03-07 14:07:19 +0200

    adj. Unlock_Object to new namespace Lock

--- include/Unlock_Object.hpp
+++ include/Unlock_Object.hpp
@@ -4,7 +4,7 @@
 
 class Unlock_Object {
 public:
-    Unlock_Object(unlock_token* _utoken, bool _enabled = true);
+    Unlock_Object(Lock::unlock_token* _utoken, bool _enabled = true);
     virtual ~Unlock_Object();
     void enable() { this->enabled = true; }
     void disable() { this->enabled = false; }
@@ -15,14 +15,15 @@ public:
         for fingerprint/rfid/pin) and pushes a unlock_request...
     */
     virtual void loop() = 0; // need to be overwritten by inherrited classes
+protected:
+    Lock::unlock_token* utoken; // for requesting to unlock the Lock
 private:
     bool enabled;   // wether we want to use this unlock_method/unlock_object
-    unlock_token* utoken; // for requesting to unlock the Lock
 };
 
 // ------------- Implementation -------------
 
-Unlock_Object::Unlock_Object(unlock_token* _utoken, bool _enabled) :utoken(_utoken), enabled(_enabled) { }
+Unlock_Object::Unlock_Object(Lock::unlock_token* _utoken, bool _enabled) :utoken(_utoken), enabled(_enabled) { }
 
 Unlock_Object::~Unlock_Object() {
     delete utoken;
