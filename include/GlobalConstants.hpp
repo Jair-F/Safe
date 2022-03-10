a5f66a55201c5de9bbd660a48a5e2809c899af97
Jair-F <jair.fehlauer@gmail.com>
2022-03-10 19:12:24 +0200

    added global constants file

---
+++ include/GlobalConstants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// Serial-Object (for fingerprint)
+#define SERIAL_RECEIVE_PIN 2
+#define SERIAL_TRANSMIT_PIN 3
+
+// RFID
+#define MFRC522_RST_PIN 49 // RST_PIN
+#define MFRC522_SS_PIN 53  // SDA_PIN
+
+// Fingerprint
