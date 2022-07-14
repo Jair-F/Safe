416019b074c7568f78d0022045657b918f2074f8
Jair-F <jair.fehlauer@gmail.com>
2022-07-14 14:59:06 +0300

    test for new Input_Field

--- include/UI/lock_screen.hpp
+++ include/UI/lock_screen.hpp
@@ -9,27 +9,31 @@ UI::position lower_right{130, 70};
 class lock_screen : public UI::Window
 {
 public:
-    lock_screen(UI::MainWindow *_main_window) : UI::Window(_main_window), _button(this, upper_left, lower_right, this),
+    lock_screen(UI::MainWindow *_main_window) : UI::Window(_main_window),
+                                                _button(this, upper_left, lower_right, this),
                                                 _button2(this, {this->_get_display()->getDisplayYSize() - 100, this->_get_display()->getDisplayXSize() - 35}, {this->_get_display()->getDisplayYSize() - 1, this->_get_display()->getDisplayXSize() - 1}, this),
                                                 text_feld(this, {0, 0}, this->_get_display()->getDisplayYSize() - 1, "Window title"),
-                                                ch_box(this, {50, 150}, 25, 25, this)
+                                                ch_box(this, {50, 150}, 25, 25, this),
+                                                input_field(this, {150, 150}, 60, 35, this, UI::InputField<10, '*', lock_screen>::IN_INPUT_TYPE::IN_DIGIT)
     {
         text_feld.set_border(false);
         text_feld.set_text_alignment(text_feld.AL_CENTER);
         text_feld.set_font(SmallFont);
 
         _button.setText("Click mich");
-
         _button.on_click = &this->button_print_clicked;
         _button.on_release = &this->button_print_released;
 
         _button2.on_release = &this->restart_system;
-
         _button2.setText("Restart");
 
         ch_box.on_release = &this->_handle_check_box;
-        ch_box.check_sign_color = VGA_RED;
+        ch_box.check_sign_color = VGA_WHITE;
+        ch_box.background_color = VGA_GREEN;
+        ch_box.border_color = VGA_AQUA;
         ch_box.set_checked(false);
+
+        input_field.set_input_buffer("123");
     }
     virtual ~lock_screen() {}
 
@@ -87,6 +91,7 @@ private:
     UI::Button<lock_screen> _button2;
     UI::TextLabel text_feld;
     UI::CheckBox<lock_screen> ch_box;
+    UI::InputField<10, '*', lock_screen> input_field;
 };
 
 void lock_screen::show()
@@ -95,6 +100,7 @@ void lock_screen::show()
     _button2.show();
     text_feld.show();
     ch_box.show();
+    input_field.show();
 }
 
 void lock_screen::hide()
@@ -102,6 +108,7 @@ void lock_screen::hide()
     _button.hide();
     _button2.hide();
     text_feld.hide();
+    input_field.hide();
 }
 
 void lock_screen::loop()
