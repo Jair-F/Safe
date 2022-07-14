e8202534d874729473ffbb7183634f172ca2d399
Jair-F <jair.fehlauer@gmail.com>
2022-07-14 14:56:41 +0300

    implementation of input_field

--- include/UI/UI.hpp
+++ include/UI/UI.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "lib/position.hpp"
+#include "lib/MainWindow.hpp"
 #include "lib/Window.hpp"
 #include "lib/Widget.hpp"
 #include "lib/Button.hpp"
 #include "lib/TextLabel.hpp"
 #include "lib/CheckBox.hpp"
+#include "lib/InputField.hpp"
---
+++ include/UI/lib/InputField.hpp
@@ -0,0 +1,316 @@
+#pragma once
+
+#include "Touch_Widget.hpp"
+#include "Window.hpp"
+
+namespace UI
+{
+    /*
+        @param MAX_NUM_OF_CHARS maximum number of input-digits/chars/letters that the input_field can storage and will accept. If the user
+                tries to input more the field will simply not accept more.
+    */
+    template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+    class InputField : public Touch_Widget<CALL_OBJECT_TYPE>
+    {
+    public:
+        void (CALL_OBJECT_TYPE::*on_enter)(Widget *_widget);
+
+        enum class IN_INPUT_TYPE
+        {
+            IN_DIGIT,
+            IN_PASSWORD
+        };
+
+        InputField(Window *_parent, const position _upper_left_pos, const position _lower_right_pos,
+                   CALL_OBJECT_TYPE *_call_object, IN_INPUT_TYPE _input_type);
+        InputField(Window *_parent, const position _upper_left_pos, uint16_t _width, uint16_t _height,
+                   CALL_OBJECT_TYPE *_call_object, IN_INPUT_TYPE _input_type);
+
+        // function called from the keypad from the MainWindow
+        void send_input(char _input_data) override;
+
+        /*
+            @return pointer to the input_buffer of the class - make sure that the instance of the InputField class exists at accessing the data!!!
+        */
+        const char *get_input_buffer() { return this->input_buffer; }
+        void set_input_buffer(String _input_data);
+        void set_input_buffer(const char *_input_data);
+        void clear_input_buffer();
+
+        /*
+            @return true if the buffer is empty
+        */
+        bool buffer_is_empty();
+
+        void set_font(uint8_t *_font) { this->_text_font = _font; }
+        uint8_t *get_font() { return this->_text_font; }
+
+    protected:
+        void _draw_released_widget() override;
+        /*
+            at this time this isnt when the widget is pressed. its when the widget is focused and ready for input - then we show the cursor
+        */
+        void _draw_pressed_widget() override;
+        void _draw_widget() override;
+
+        void _focus_lose() override;
+
+    private:
+        char input_buffer[MAX_NUM_OF_CHARS];
+
+        IN_INPUT_TYPE input_type;
+
+        /*
+            the value which is set to the input_buffer[i] if it is not set via the input. - we know the size of the input_buffer-array so
+            we can set it to '\0' to indicate that this element is not set
+        */
+        static constexpr char INPUT_UNSET_VALUE = '\0';
+
+        /*
+            the color values are RGB-565 values(16-bit value)
+            RGB-565 color picker: https://chrishewett.com/blog/true-rgb565-colour-picker/
+        */
+        unsigned int pressed_background_color = 0xb5b6;
+        unsigned int pressed_text_color = VGA_BLACK;
+        unsigned int pressed_cursor_color = VGA_BLACK;
+        unsigned int pressed_border_color = VGA_WHITE;
+
+        unsigned int released_background_color = VGA_BLACK;
+        unsigned int released_text_color = VGA_WHITE;
+        unsigned int released_border_color = VGA_WHITE;
+
+        uint8_t *_text_font = SmallFont;
+
+        uint8_t _text_gap; // gap between the left border and the start of the text and the right border and the end of the text
+    };
+} // namespace UI
+
+// ------------- template implementation -------------
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::InputField(Window *_parent, const position _upper_left_pos, const position _lower_right_pos,
+                                                                              CALL_OBJECT_TYPE *_call_object, IN_INPUT_TYPE _input_type) : Touch_Widget<CALL_OBJECT_TYPE>(_parent, _upper_left_pos, _lower_right_pos, _call_object),
+                                                                                                                                           input_type(_input_type)
+{
+    assert(MAX_NUM_OF_CHARS > 0);
+
+    this->_text_gap = 0.5 * this->display->getFontXsize(); // 1/2 of the font width
+
+    // initializing input_buffer with default unset values
+    for (uint8_t i = 0; i < MAX_NUM_OF_CHARS; ++i)
+    {
+        this->input_buffer[i] = this->INPUT_UNSET_VALUE;
+    }
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::InputField(Window *_parent, const position _upper_left_pos, uint16_t _width, uint16_t _height, CALL_OBJECT_TYPE *_call_object,
+                                                                              IN_INPUT_TYPE _input_type) : InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>(_parent, _upper_left_pos, {_upper_left_pos.x_pos + _width, _upper_left_pos.y_pos + _height}, _call_object, _input_type)
+{
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::send_input(char _input_data)
+{
+    if (_input_data != this->INPUT_UNSET_VALUE) // INPUT_UNSET_VALUE is not allowed...
+    {
+        switch (_input_data)
+        {
+        case (BACKSPACE_KEY):
+        {
+            // searching for the last set input-pos
+            uint8_t i = 0;
+            for (; i < MAX_NUM_OF_CHARS; ++i)
+            {
+                if (this->input_buffer[i] == this->INPUT_UNSET_VALUE)
+                {
+                    break;
+                }
+            }
+            --i;
+            this->input_buffer[i] = this->INPUT_UNSET_VALUE;
+
+            break;
+        }
+
+        // add input to the buffer
+        default:
+        {
+
+            for (uint8_t i = 0; i < MAX_NUM_OF_CHARS; ++i)
+            {
+                if (this->input_buffer[i] == this->INPUT_UNSET_VALUE)
+                {
+                    this->input_buffer[i] = _input_data;
+                    break;
+                }
+            }
+            break;
+        }
+        }
+        this->_draw_widget();
+    }
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::clear_input_buffer()
+{
+    for (uint8_t i = 0; i < MAX_NUM_OF_CHARS; ++i)
+    {
+        this->input_buffer[i] = this->INPUT_UNSET_VALUE;
+    }
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::set_input_buffer(const char *_input_data)
+{
+    size_t _input_data_length = strlen(_input_data);
+
+    uint8_t i = 0;
+    for (; i < (_input_data_length < MAX_NUM_OF_CHARS ? _input_data_length : MAX_NUM_OF_CHARS); ++i) // ensure that we dont go over _input_data buffer/length and not over this->input_buffer
+    {
+        this->input_buffer[i] = _input_data[i];
+    }
+
+    // filling up the buffer with unset_values from the position we stopped above and continue until the end of the input_buffer
+    for (; i < MAX_NUM_OF_CHARS; ++i)
+    {
+        this->input_buffer[i] = this->INPUT_UNSET_VALUE;
+    }
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::set_input_buffer(String _input_buffer)
+{
+    this->set_input_buffer(_input_buffer.c_str());
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+bool UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::buffer_is_empty()
+{
+    // we fill the buffer from pos-0 to ...
+    return this->input_buffer[0] == this->INPUT_UNSET_VALUE;
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::_draw_released_widget()
+{
+    // draw background-color
+    this->display->setColor(this->released_background_color);
+    this->display->fillRect(this->_upper_left_pos.x_pos, this->_upper_left_pos.y_pos,
+                            this->_lower_right_pos.x_pos, this->_lower_right_pos.y_pos);
+
+    // draw the border
+    this->display->setColor(this->released_border_color);
+    this->display->drawRect(this->_upper_left_pos.x_pos, this->_upper_left_pos.y_pos,
+                            this->_lower_right_pos.x_pos, this->_lower_right_pos.y_pos);
+
+    // print the print the text in the InputField if there is text in the buffer
+    if (!this->buffer_is_empty())
+    {
+        uint8_t font_height = this->display->getFontYsize();
+        uint8_t font_width = this->display->getFontXsize();
+
+        this->display->setFont(this->_text_font);
+        this->display->setBackColor(this->released_background_color);
+        this->display->setColor(this->released_text_color);
+
+        // calculate the num of chars to print in the field - in case there is more input than the widget ca display
+        uint8_t input_buffer_size = strlen(this->input_buffer);
+
+        uint8_t space_for_text = (this->width() - this->_text_gap * 2); // in pixels
+        uint8_t num_of_chars_to_print = space_for_text / font_width;    // num of characters we can print in the widget
+        String text_to_print;
+
+        if (input_buffer_size < num_of_chars_to_print) // if the field isnt filled up entirely print out the text just like normal
+        {
+            text_to_print = this->input_buffer;
+        }
+        else
+        {
+            for (signed int i = num_of_chars_to_print; i > 0; --i)
+            {
+                text_to_print += this->input_buffer[input_buffer_size - i];
+            }
+        }
+
+        // center the text horizontally in the widget
+        this->display->print(text_to_print, this->_upper_left_pos.x_pos + this->_text_gap,
+                             this->_upper_left_pos.y_pos + (this->height() / 2) - font_height / 2); // this->INPUT_UNSET_VALUE is '\0' - end of text...
+    }
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::_draw_pressed_widget()
+{
+    // draw background-color
+    this->display->setColor(this->pressed_background_color);
+    this->display->fillRect(this->_upper_left_pos.x_pos, this->_upper_left_pos.y_pos,
+                            this->_lower_right_pos.x_pos, this->_lower_right_pos.y_pos);
+
+    // draw the border
+    this->display->setColor(this->pressed_border_color);
+    this->display->drawRect(this->_upper_left_pos.x_pos, this->_upper_left_pos.y_pos,
+                            this->_lower_right_pos.x_pos, this->_lower_right_pos.y_pos);
+
+    uint8_t font_height = this->display->getFontYsize();
+    uint8_t font_width = this->display->getFontXsize();
+
+    String text_to_print; // the text to print int the widget
+
+    // print the print the text in the InputField if there is text in the buffer
+    if (!this->buffer_is_empty())
+    {
+        this->display->setFont(this->_text_font);
+        this->display->setBackColor(this->pressed_background_color);
+        this->display->setColor(this->pressed_text_color);
+
+        // calculate the num of chars to print in the field - in case there is more input than the widget ca display
+        uint8_t input_buffer_size = strlen(this->input_buffer);
+
+        uint8_t space_for_text = (this->width() - this->_text_gap * 2); // in pixels
+        uint8_t num_of_chars_to_print = space_for_text / font_width;    // num of characters we can print in the widget
+
+        if (input_buffer_size < num_of_chars_to_print) // if the field isnt filled up entirely print out the text just like normal
+        {
+            text_to_print = this->input_buffer;
+        }
+        else
+        {
+            for (signed int i = num_of_chars_to_print; i > 0; --i)
+            {
+                text_to_print += this->input_buffer[input_buffer_size - i];
+            }
+        }
+
+        // center the text horizontally in the widget
+        this->display->print(text_to_print, this->_upper_left_pos.x_pos + this->_text_gap,
+                             this->_upper_left_pos.y_pos + (this->height() / 2) - font_height / 2); // this->INPUT_UNSET_VALUE is '\0' - end of text...
+    }
+    // draw the cursor
+    this->display->setColor(this->pressed_cursor_color);
+    this->display->drawVLine(this->_upper_left_pos.x_pos + this->_text_gap + font_width * text_to_print.length(),
+                             this->_upper_left_pos.y_pos + (this->height() / 2) - font_height / 2, font_height); // height of the curser is the font-height and the upper-pos is the upper-pos of the text
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::_focus_lose()
+{
+    this->_draw_released_widget();
+}
+
+template <uint8_t MAX_NUM_OF_CHARS, char BACKSPACE_KEY, typename CALL_OBJECT_TYPE>
+void UI::InputField<MAX_NUM_OF_CHARS, BACKSPACE_KEY, CALL_OBJECT_TYPE>::_draw_widget()
+{
+    if (!this->is_hidden())
+    {
+        if (this->is_touched() || this->is_focused())
+        {
+            this->_draw_pressed_widget();
+        }
+        else
+        {
+            this->_draw_released_widget();
+        }
+    }
+}
