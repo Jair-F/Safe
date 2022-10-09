591f5026d368a7947b390f0ac0deaf3b6b0f8b32
Jair-F <jair.fehlauer@gmail.com>
2022-10-09 10:18:27 +0300

    refactor of RFID

--- include/RFID/RFID.hpp
+++ include/RFID/RFID.hpp
@@ -2,7 +2,6 @@
 
 #include <Arduino.h>
 #include <MFRC522.h>
-#include <Adafruit_PN532.h>
 #include "../Unlock_Object.hpp"
 #include "../Helper.hpp"
 #include "UID.hpp"
@@ -24,74 +23,84 @@ namespace RFID
     class RFID : public Unlock_Object
     {
     public:
-        /*
-            @param reset not connected by default
-        */
-        RFID(byte irq, byte reset, Lock *_lock, bool _enabled = true);
+        /**
+         * @param rst defined reset pin for the sensor
+         * @param ss slave select pin
+         *
+         * @note SPI must be initialized before begin is called!!
+         */
+        RFID(byte ss, byte rst, Lock *_lock, bool _enabled = true);
         virtual ~RFID() {}
 
-        /*
-            @return true if the RFID-PN532 was setup successfully - else false
-        */
+        /**
+         * @return true if the RFID was setup successfully
+         */
         bool begin();
 
-        /*
-            read for a tag id
-            @return the tag id if a tag was present else an empty UID-object
-        */
+        /**
+         * @brief read for a tag id
+         * @return the tag id if a tag was present else an empty UID-object - check with is_set()
+         */
         UID read_tag_UID();
 
         bool tag_present();
 
-        /*
-            reads from the RFID_module for a tag and adds it to database at id id
-            !!! this function reads only one time - if there was no tag it will return without doing nothing !!!
-            Maybe you want to call this function in a loop to ensure that a tag was added
-            @param id id where to store in the database - it overwrites if there exists already a tag
-            @return true if a tag was added else false
-        */
+        /**
+         * @details reads from the RFID_module for a tag and adds it to database at id id
+         * !!! this function reads only one time - if there was no tag it will return without doing nothing !!!
+         * Maybe you want to call this function in a loop to ensure that a tag was added
+         * @param id id where to store in the database - it overwrites if there exists already a tag
+         * @return true if a tag was added else false
+         */
         bool read_add_tag(unsigned short id);
-        /*
-            adds a tag to the allowed_tags list
-        */
+        /**
+         * @brief adds a tag to the allowed_tags list
+         */
         void add_tag(unsigned short id, UID tag_uid);
-        /*
-            removes tag from the tag with id id from the allowed_tags
-            throws an error if the id id is out of range
-        */
+        /**
+         * @brief removes tag from the tag with id id from the allowed_tags
+         * (throws an error if the id id is out of range)??
+         */
         void remove_tag(unsigned short id);
-        /*
-            removes tag from the tag with uidid tag_uid from the allowed_tags
-            @return true if one or more tags where removed - else false
-        */
+        /**
+         * @brief removes tag from the tag with UID tag_uid from the allowed_tags
+         * @return true if one or more tags where removed - else false
+         */
         bool remove_tag(const UID &tag_uid);
-        /*
-            removes all saved tags from the allowed_tags_database
-        */
+        /**
+         * @brief removes all saved tags from the allowed_tags_database
+         */
         void clear_database();
 
-        /*
-            @return the tag uid if the tag is set else it throws an error
-        */
+        /**
+         * @return the tag uid if the tag is set else an empty UID - check with is_set()
+         */
         UID &get_tag_uid(unsigned short id);
-        /*
-            @param the tag_uid to get the id from - has to be with whitespaces removed from left
-                    and right and has to be in the format like the get_tag_uid() returns("xx xx xx xx")
-            @return if a matching tag was found the tag_uid(>0). if no tag was found -1
-        */
+        /**
+         * @param the tag_uid to get the id from - has to be with whitespaces removed from left
+         *         and right and has to be in the format like the get_tag_uid() returns("xx xx xx xx")
+         * @return if a matching tag was found the tag_uid(>0). if no tag was found -1
+         */
         int get_tag_id(UID tag_uid);
-        // get_used_ids
+
+        /**
+         * @return true if on that position an UID is saved
+         */
         bool id_used(unsigned short id) const;
         // bool in_database(String uid);
 
-        /*
-            read for uids and return a succes if a uid was read that matches to one of the database
-        */
+        /**
+         * @brief read for uids and return a succes if a uid was read that matches to one of the database -
+         * for the Lock class...
+         */
         unlock_authentication_reports read() override;
 
     private:
-        Adafruit_PN532 rfid;
+        MFRC522 rfid;
         UID allowed_tags[NUM_OF_TAGS];
+
+        // if we detected a tag which UID isnt already read - PICC_IsNewCardPresent can only be called once for every tag read
+        bool unread_tag_present = false;
     };
 
 } // namespace RFID
--- src/RFID/UID.cpp
+++ src/RFID/UID.cpp
@@ -1,15 +1,35 @@
 #include "RFID/UID.hpp"
 
-RFID::UID::UID(uint8_t *_uid, uint8_t _uid_length) : uid_length(_uid_length)
+RFID::UID::UID(uint8_t *_uid, uint8_t _uid_length) : uid_length(_uid_length), uid(new uint8_t[_uid_length])
 {
     for (uint8_t i = 0; i < _uid_length; ++i)
     {
         this->uid[i] = _uid[i];
     }
 }
-RFID::UID::UID(const UID &_uid) : uid_length(_uid.get_uid_length())
+RFID::UID::UID(const UID &_uid) : uid_length(_uid.get_uid_length()), uid(new uint8_t[uid_length])
 {
     for (uint8_t i = 0; i < uid_length; ++i)
+    {
+        this->uid[i] = _uid.cget_uid()[i];
+    }
+}
+
+RFID::UID::UID(UID &&_uid) : uid(_uid.get_uid()), uid_length(_uid.get_uid_length())
+{
+}
+
+RFID::UID::~UID()
+{
+    if (this->uid != nullptr)
+        delete[] this->uid;
+}
+
+RFID::UID &RFID::UID::operator=(const UID &_uid)
+{
+    this->uid_length = _uid.get_uid_length();
+    this->uid = new uint8_t[this->uid_length];
+    for (uint8_t i = 0; i < this->uid_length; ++i)
     {
         this->uid[i] = _uid.cget_uid()[i];
     }
@@ -50,14 +70,12 @@ RFID::UID::operator bool()
 
 bool RFID::UID::is_set() const
 {
-    return this->uid_length != 0;
+    return this->uid_length != 0 && this->uid != nullptr;
 }
 
 void RFID::UID::clear()
 {
     this->uid_length = 0;
-    for (unsigned short i = 0; i < MAX_UID_BLOCKS; ++i)
-    {
-        this->uid[i] = 0;
-    }
+    delete[] this->uid;
+    this->uid = nullptr;
 }
